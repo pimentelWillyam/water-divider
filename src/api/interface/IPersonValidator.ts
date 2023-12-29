28204dafca9c9acff2499cbe023d5471e91d2ce4
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-12-29 12:08:08 -0300

    refactor: changing return type from person validator interface

--- src/api/interface/IPersonValidator.ts
+++ src/api/interface/IPersonValidator.ts
@@ -1,6 +1,8 @@
+import type KnownError from '../validator/errors/KnownError'
+
 interface ILogValidator {
-  validateCreation: (name: string, email: string, age: number) => void
-  validateUpdate: (name: string, email: string, age: number) => void
+  validateCreation: (name: string, email: string, age: number) => KnownError[]
+  validateUpdate: (name: string, email: string, age: number) => KnownError[]
 
 }
 
