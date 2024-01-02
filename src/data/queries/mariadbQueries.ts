ee0ed7b90337d096be0c11a1e999ef4f8b362428
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-02 15:21:51 -0300

    feat: sleepy and leaving usa

--- src/data/queries/mariadbQueries.ts
+++ src/data/queries/mariadbQueries.ts
@@ -1,4 +1,7 @@
 const mariadbQueries = {
+  createDatabase: 'CREATE DATABASE ?;',
+  useDatabase: 'USE ?;',
+  fetchDatabase: 'SHOW DATABASES LIKE ? ;',
   createPersonTable: `CREATE TABLE person (
     id VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
@@ -6,13 +9,22 @@ const mariadbQueries = {
     age INT NOT NULL,
     PRIMARY KEY (id)
   );`,
+  fetchTable: 'SHOW TABLES FROM ? LIKE "?";',
 
-  insertPersonRegistry: 'INSERT INTO person (id, name, email, age)',
+  insertPersonRegistry: 'INSERT INTO person (id, name, email, age);',
 
-  fetchEveryPersonRegistry: 'SELECT * FROM boilerplate.person ;',
+  fetchEveryPersonRegistry: 'SELECT * FROM boilerplate.person;',
 
-  fetchPersonRegistryByParameter: 'SELECT * FROM boilerplate.person WHERE '
+  fetchPersonRegistryBy: 'SELECT * FROM boilerplate.person WHERE ? = ?;',
 
+  updatePersonRegistryBy: `UPDATE boilerplate.person SET
+  id = ?,
+  name = ?,
+  email = ?,
+  age = ?
+  WHERE id = ?;`,
+
+  deletePersonRegistryBy: 'DELETE FROM boilerplate.person WHERE ? = ?\';'
 }
 
 export { mariadbQueries }
