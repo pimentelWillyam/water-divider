af9bccd7a74f1e0fdd7e9020b6d3f4aaf34e9173
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-03 15:57:13 -0300

    refactor: adding promises

--- src/api/interface/IMemoryDataSource.ts
+++ src/api/interface/IMemoryDataSource.ts
@@ -2,8 +2,8 @@ import { type DatabasePerson } from '../../data/model/DatabasePerson'
 import type Person from '../entity/Person'
 
 export interface IMemoryDataSource {
-  start: () => void
-  stop: () => void
+  start: () => Promise<void>
+  stop: () => Promise<void>
   insertPersonRegistry: (person: Person) => Promise<DatabasePerson>
   fetchEveryPersonRegistry: () => Promise<DatabasePerson[]>
   fetchPersonBy: (parameter: string, parameterValue: string) => Promise<Person | null>
