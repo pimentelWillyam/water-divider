4e24373666ebe4132cc9c9c4bb214382e0a0e907
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-03 15:59:23 -0300

    refactor: renaming queries title

--- src/data/queries/mariadbQueries.ts
+++ src/data/queries/mariadbQueries.ts
@@ -1,21 +1,18 @@
 const mariadbQueries = {
-  createDatabase: 'CREATE DATABASE ?;',
-  useDatabase: 'USE ?;',
-  fetchDatabase: 'SHOW DATABASES LIKE ? ;',
-  createPersonTable: `CREATE TABLE person (
+  createBoilerplateDatabase: 'CREATE DATABASE IF NOT EXISTS boilerplate ;',
+  useBoilerplateDatabase: 'USE boilerplate ;',
+  createPersonTable: `CREATE TABLE IF NOT EXISTS person (
     id VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     age INT NOT NULL,
     PRIMARY KEY (id)
   );`,
-  fetchTable: 'SHOW TABLES FROM ? LIKE "?";',
-
-  insertPersonRegistry: 'INSERT INTO person (id, name, email, age);',
+  insertPersonRegistry: 'INSERT INTO person (id, name, email, age) VALUES (?,?,?,?);',
 
   fetchEveryPersonRegistry: 'SELECT * FROM boilerplate.person;',
 
-  fetchPersonRegistryBy: 'SELECT * FROM boilerplate.person WHERE ? = ?;',
+  fetchPersonRegistryBy: 'SELECT * FROM boilerplate.person WHERE "?" = "?";',
 
   updatePersonRegistryBy: `UPDATE boilerplate.person SET
   id = ?,
@@ -24,7 +21,7 @@ const mariadbQueries = {
   age = ?
   WHERE id = ?;`,
 
-  deletePersonRegistryBy: 'DELETE FROM boilerplate.person WHERE ? = ?\';'
+  deletePersonRegistryBy: 'DELETE FROM boilerplate.person WHERE "?" = "?";'
 }
 
 export { mariadbQueries }
