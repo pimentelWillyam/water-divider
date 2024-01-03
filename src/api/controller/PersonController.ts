f9815d2c3dcbb5c51ba3cdd0375372982ae49a7b
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-03 15:54:01 -0300

    refactor: adding promises

--- src/api/controller/PersonController.ts
+++ src/api/controller/PersonController.ts
@@ -8,25 +8,28 @@ import KnownError from '../validator/errors/KnownError'
 class PersonController implements IPersonController {
   constructor (readonly personService: IPersonService, readonly personValidator: IPersonValidator) {}
 
-  create (req: Request, res: Response): Response<any, Record<string, any>> {
+  async create (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
     const errorList = this.personValidator.validateCreation(req.body.name, req.body.email, req.body.age)
     if (errorList.length !== 0) return res.status(400).json({ errorList })
     try {
-      const person = this.personService.create(req.body.name, req.body.email, req.body.age)
+      const person = await this.personService.create(req.body.name, req.body.email, req.body.age)
       return res.status(201).json(person)
     } catch (error) {
+      console.error(error)
       if (error instanceof KnownError) {
         return res.status(error.status).send({ name: error.name, message: error.message })
       }
+
       return res.status(500).send({ name: 'Erro desconhecido', message: 'Um erro inesperado aconteceu durante a requisição' })
     }
   }
 
-  getAll (res: Response): Response<any, Record<string, any>> {
+  async getAll (res: Response): Promise<Response<any, Record<string, any>>> {
     try {
-      const personList = this.personService.getAll()
+      const personList = await this.personService.getAll()
       return res.status(200).json(personList)
     } catch (error) {
+      console.error(error)
       if (error instanceof KnownError) {
         return res.status(error.status).send({ name: error.name, message: error.message })
       }
@@ -34,12 +37,13 @@ class PersonController implements IPersonController {
     }
   }
 
-  get (req: Request, res: Response): Response<any, Record<string, any>> {
+  async get (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
     try {
-      const person = this.personService.get(req.params.id)
+      const person = await this.personService.get(req.params.id)
       if (person !== null) return res.status(200).send(person)
       else return res.status(404).send({ name: 'Pessoa não encontrada', message: 'Não foi possível encontrar uma pessoa com este ID' })
     } catch (error) {
+      console.error(error)
       if (error instanceof KnownError) {
         return res.status(error.status).send({ name: error.name, message: error.message })
       }
@@ -47,14 +51,15 @@ class PersonController implements IPersonController {
     }
   }
 
-  update (req: Request, res: Response): Response<any, Record<string, any>> {
+  async update (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
     const errorList = this.personValidator.validateUpdate(req.body.name, req.body.email, req.body.age)
     if (errorList.length !== 0) return res.status(400).json({ errorList })
     try {
-      const person = this.personService.update(req.params.id, req.body)
+      const person = await this.personService.update(req.params.id, req.body)
       if (person !== null) return res.status(200).send(person)
       else return res.status(404).send({ name: 'Pessoa não encontrada', message: 'Não foi possível encontrar uma pessoa com este ID' })
     } catch (error) {
+      console.error(error)
       if (error instanceof KnownError) {
         return res.status(error.status).send({ name: error.name, message: error.message })
       }
@@ -62,12 +67,13 @@ class PersonController implements IPersonController {
     }
   }
 
-  delete (req: Request, res: Response): Response<any, Record<string, any>> {
+  async delete (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
     try {
-      const person = this.personService.delete(req.params.id)
+      const person = await this.personService.delete(req.params.id)
       if (person !== null) return res.status(200).send(person)
       else return res.status(404).send({ name: 'Pessoa não encontrada', message: 'Não foi possível encontrar uma pessoa com este ID' })
     } catch (error) {
+      console.error(error)
       if (error instanceof KnownError) {
         return res.status(error.status).send({ name: error.name, message: error.message })
       }
