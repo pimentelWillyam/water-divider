ebe7aa91d6aababd38fbc4776133801cd10514af
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-08 17:05:33 -0300

    refactor: removing useless schema

--- src/data/queries/postgresQueries.ts
+++ src/data/queries/postgresQueries.ts
@@ -4,13 +4,13 @@ const postgresQueries = {
   verifyIfBoilerplateDatabaseExists: "SELECT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'boilerplate') AS exists ;",
   verifyIfPersonTableExists: "SELECT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'boilerplate') AS exists ;",
   useBoilerplateDatabase: 'USE boilerplate ;',
-  createPersonTable: `CREATE TABLE boilerplate.person (
+  createPersonTable: `CREATE TABLE person (
       id VARCHAR(255) NOT NULL,
       name VARCHAR(255) NOT NULL,
       email VARCHAR(255) NOT NULL,
       age INT NOT NULL,
       PRIMARY KEY (id)
-    ) ;`,
+    );`,
   insertPersonRegistry: 'INSERT INTO boilerplate.person (id, name, email, age) VALUES ($1,$2,$3,$4) ;',
   fetchEveryPersonRegistry: 'SELECT * FROM boilerplate.person ;',
   fetchPersonRegistryBy: 'SELECT * FROM boilerplate.person WHERE $1 = $2 ;',
