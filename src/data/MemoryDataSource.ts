9b46b829325b72dec433edb10e087cfb001f42cf
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-08 16:58:06 -0300

    refactor: refactor:  changing interface import

--- src/data/MemoryDataSource.ts
+++ src/data/MemoryDataSource.ts
@@ -1,8 +1,8 @@
 import type Person from '../api/entity/Person'
-import { type IMemoryDataSource } from './interface/IMemoryDataSource'
+import { type IInMemoryDataSource } from './interface/IInMemoryDataSource'
 import { type DatabasePerson } from './model/DatabasePerson'
 
-class MemoryDataSource implements IMemoryDataSource {
+class MemoryDataSource implements IInMemoryDataSource {
   private personList: Person[] = []
 
   readonly start = async (): Promise<void> => {
