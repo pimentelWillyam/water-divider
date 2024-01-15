cc88ef956afde211a86e638b9c6f64ed2098858a
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-14 22:20:09 -0300

    feat: dding postgres case to builder

--- src/api/factory/DataSourceFactory.ts
+++ src/api/factory/DataSourceFactory.ts
@@ -4,6 +4,7 @@ import { type IDataSourceFactory } from '../interface/IDataSourceFactory'
 import { type DataSourceType } from '../../data/type/DataSourceType'
 import { dataSourceConfig } from '../../data/dataSourceConfig'
 import { type DataSource } from '../../data/type/Datasource'
+import { PostgresDataSource } from '../../data/PostgresDataSource'
 
 class DataSourceFactory implements IDataSourceFactory {
   fabricate (dataSourceType: DataSourceType): DataSource {
@@ -13,6 +14,9 @@ class DataSourceFactory implements IDataSourceFactory {
 
       case 'mariadb':
         return new MariadbDataSource(dataSourceConfig.mariadb.host, dataSourceConfig.mariadb.port, dataSourceConfig.mariadb.user, dataSourceConfig.mariadb.password, dataSourceConfig.mariadb.connectionLimit)
+
+      case 'postgres':
+        return new PostgresDataSource(dataSourceConfig.postgres)
       default:
         throw new Error('Error during data source creation')
     }
