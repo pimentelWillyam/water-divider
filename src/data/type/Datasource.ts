47f4d6488c5327dbe79782f57553558cd6b32ab9
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-14 22:21:13 -0300

    feat: adding psotgres datasource type

--- src/data/type/Datasource.ts
+++ src/data/type/Datasource.ts
@@ -1,6 +1,7 @@
 import type MemoryDataSource from '../MemoryDataSource'
 import type MariadbDataSource from '../MariadbDataSource'
+import { type PostgresDataSource } from '../PostgresDataSource'
 
-type DataSource = MemoryDataSource | MariadbDataSource
+type DataSource = MemoryDataSource | MariadbDataSource | PostgresDataSource
 
 export type { DataSource }
