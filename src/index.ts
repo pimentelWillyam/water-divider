3c0288d1aa44b8dc5ed4ee7e2d190f90c49d68c0
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-25 14:06:16 -0300

    refactor: changing to memory

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { APIFactory } from './api/factory/APIFactory'
 import { DataSourceFactory } from './api/factory/DataSourceFactory'
+import {} from 'dotenv-safe'
+
+dotenv.config()
 
 class Main {
   async start (): Promise<void> {
-    const dataSource = new DataSourceFactory().fabricate('mariadb')
+    const dataSource = new DataSourceFactory().fabricate('memory')
     const api = new APIFactory(dataSource).fabricate()
     api.start(4000)
     await dataSource.start()
