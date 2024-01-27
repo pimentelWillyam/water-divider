6ec4eb737dcceb4e08a5f7809bd5a3de219f2c18
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-27 14:06:32 -0300

    refactor: moving datasource config to config file

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,26 @@
 const config = {
-  api: {
-    host: process.env.HOST_API ?? 'localhost',
-    port: process.env.PORT_API ?? 4000
+  dataSource: {
+    mariadb: {
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'mariadb',
+      connectionLimit: 5
+    },
+    postgres: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'postgres',
+      port: 5432,
+      connectionLimit: 5
+
+    },
+    api: {
+      host: process.env.HOST_API ?? 'localhost',
+      port: process.env.PORT_API ?? 4000
+    }
   }
+
 }
 
-export default config
+export { config }
