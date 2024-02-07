329ce4b3378d0177d67831729a38335665c6100f
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-07 16:15:47 -0300

    feat: adding auth controller

---
+++ src/api/controller/AuthController.ts
@@ -0,0 +1,25 @@
+import { type Request, type Response } from 'express'
+
+import type IAuthController from '../interface/IAuthController'
+import type IAuthService from '../interface/IAuthService'
+import KnownError from '../validator/errors/KnownError'
+
+class AuthController implements IAuthController {
+  constructor (readonly authService: IAuthService) {}
+
+  async authenticate (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    try {
+      const auth = await this.authService.authenticate(req.body.login, req.body.password)
+      return res.status(200).json(auth)
+    } catch (error) {
+      console.error(error)
+      if (error instanceof KnownError) {
+        return res.status(error.status).send({ name: error.name, message: error.message })
+      }
+
+      return res.status(500).send({ name: 'Erro desconhecido', message: 'Um erro inesperado aconteceu durante a requisição' })
+    }
+  }
+}
+
+export { AuthController }
