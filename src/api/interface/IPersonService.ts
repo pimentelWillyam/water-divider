a4113d7fd3f10357d1fbde624518c13336bd2e96
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-09 14:49:03 -0300

    refactor: adding login and password to person service interface

--- src/api/interface/IPersonService.ts
+++ src/api/interface/IPersonService.ts
@@ -1,9 +1,10 @@
 import type Person from '../entity/Person'
 
 interface IPersonService {
-  create: (name: string, email: string, age: number) => Promise<Person>
+  create: (login: string, password: string, name: string, email: string, age: number) => Promise<Person>
   getAll: () => Promise<Person[]>
   get: (id: string) => Promise<Person | null>
+  getByLogin: (login: string) => Promise<Person | null>
   update: (id: string, personToBeUpdated: Person) => Promise<Person | null>
   delete: (id: string) => Promise<Person | null>
 
