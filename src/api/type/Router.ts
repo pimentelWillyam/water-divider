629117b713d4c953aa6e02acde283fd0041ec3fd
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-09 14:52:00 -0300

    feat: adding authrouter to router types

--- src/api/type/Router.ts
+++ src/api/type/Router.ts
@@ -1,5 +1,6 @@
+import type AuthRouter from '../router/AuthRouter'
 import type PersonRouter from '../router/PersonRouter'
 
-type Router = PersonRouter
+type Router = PersonRouter | AuthRouter
 
 export type { Router }
