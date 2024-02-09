0526995629cff45924e8e3ec6c8674054b786312
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-09 14:49:26 -0300

    refactor: adding login and password to person validator interface

--- src/api/interface/IPersonValidator.ts
+++ src/api/interface/IPersonValidator.ts
@@ -1,9 +1,9 @@
 import type KnownError from '../validator/errors/KnownError'
 
-interface ILogValidator {
-  validateCreation: (name: string, email: string, age: number) => KnownError[]
-  validateUpdate: (name: string, email: string, age: number) => KnownError[]
+interface IPersonValidator {
+  validateCreation: (login: string, password: string, name: string, email: string, age: number) => KnownError[]
+  validateUpdate: (login: string, password: string, name: string, email: string, age: number) => KnownError[]
 
 }
 
-export default ILogValidator
+export default IPersonValidator
