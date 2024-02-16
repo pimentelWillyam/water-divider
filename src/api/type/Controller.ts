84f1de6ac8d2fac3add6233bfa482220fb14d4eb
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-15 21:20:47 -0300

    feat: adding controller type

--- src/api/type/Controller.ts
+++ src/api/type/Controller.ts
@@ -1,5 +1,6 @@
+import { type AuthController } from '../controller/AuthController'
 import type PersonController from '../controller/PersonController'
 
-type Controller = PersonController
+type Controller = PersonController | AuthController
 
 export { type Controller }
