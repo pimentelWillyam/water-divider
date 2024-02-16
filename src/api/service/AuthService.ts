0e888d872345babfbc0106b97e57f6ab018851f6
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-15 21:20:40 -0300

    feat; \xxing auth service

--- src/api/service/AuthService.ts
+++ src/api/service/AuthService.ts
@@ -1,15 +1,16 @@
-import { type NameNormalizer } from '../helper/NameNormalizer'
 import type AuthResponse from '../entity/AuthResponse'
 import type IAuthService from '../interface/IAuthService'
 import type IPersonRepository from '../interface/IPersonRepository'
 import { type IJsonWebTokenGenerator } from '../interface/IJsonWebTokenGenerator'
+import { type IErrorFactory } from '../interface/IErrorFactory'
 
 class AuthService implements IAuthService {
-  constructor (readonly personRepository: IPersonRepository, readonly fullNameNormalizer: NameNormalizer, readonly jsonWebTokenGenerator: IJsonWebTokenGenerator) {}
+  constructor (readonly errorFactory: IErrorFactory, readonly personRepository: IPersonRepository, readonly jsonWebTokenGenerator: IJsonWebTokenGenerator) {}
 
-  async authenticate (login: string): Promise<AuthResponse> {
+  async authenticate (login: string, password: string): Promise<AuthResponse> {
     const person = await this.personRepository.getByLogin(login)
-    if (person === null) throw new Error('Valor inesperado para variável person')
+    console.log(person)
+    if (person === null || person.login !== login || person.password !== password) throw this.errorFactory.create('invalid login or password')
     return {
       id: person.id,
       name: person.name,
