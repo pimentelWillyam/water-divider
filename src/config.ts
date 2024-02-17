41c841ff025bfd8db0c531e5f6259c7ad209b9c8
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-17 11:59:56 -0300

    refactor: removing ?? from environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,27 @@
+import * as dotenv from 'dotenv-safe'
+
+dotenv.config()
+
 const config = {
   dataSource: {
     mariadb: {
-      host: process.env.MARIADB_HOST ?? 'localhost',
-      port: parseInt(process.env.MARIADB_PORT as string) ?? 3306,
-      user: process.env.MARIADB_USER ?? 'root',
-      password: process.env.MARIADB_PASSWORD ?? 'mariadb',
+      host: process.env.MARIADB_HOST as string,
+      port: parseInt(process.env.MARIADB_PORT as string),
+      user: process.env.MARIADB_USER as string,
+      password: process.env.MARIADB_PASSWORD as string,
       connectionLimit: parseInt(process.env.MARIADB_CONNECTION_LIMIT as string) ?? 5
     },
     postgres: {
-      host: process.env.MARIADB_HOST ?? 'localhost',
-      port: parseInt(process.env.MARIADB_PORT as string) ?? 5432,
-      user: process.env.MARIADB_USER ?? 'postgres',
-      password: process.env.MARIADB_PASSWORD ?? 'postgres',
-      connectionLimit: parseInt(process.env.MARIADB_CONNECTION_LIMIT as string) ?? 5
+      host: process.env.POSTGRES_HOST as string,
+      port: parseInt(process.env.MARIADB_PORT as string),
+      user: process.env.POSTGRES_USER as string,
+      password: process.env.POSTGRES_PASSWORD as string,
+      connectionLimit: parseInt(process.env.POSTGRES_CONNECTION_LIMIT as string)
 
     },
     api: {
-      host: process.env.HOST_API ?? 'localhost',
-      port: process.env.PORT_API ?? 4000
+      host: process.env.HOST_API as string,
+      port: process.env.PORT_API as string
     }
   }
 
