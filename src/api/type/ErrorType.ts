2b161e96459aa979b25af27794c9127fd9e0dfc9
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-22 21:42:44 -0300

    feat: adding new error type

--- src/api/type/ErrorType.ts
+++ src/api/type/ErrorType.ts
@@ -1,4 +1,4 @@
 type ErrorType = 'invalid age' | 'invalid data type' | 'invalid email' | 'name has any number' | 'name length below four letters' | 'invalid login or password'
-| 'login already exists' | 'email already exists' | 'email does not exists'
+| 'login already exists' | 'email already exists' | 'email does not exists' | 'invalid token'
 
 export type { ErrorType }
