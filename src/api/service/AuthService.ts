214b45675fe38a39ee1102521d938ab69543af79
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-22 21:42:34 -0300

    feat: adding jwt to auth service

--- src/api/service/AuthService.ts
+++ src/api/service/AuthService.ts
@@ -1,11 +1,11 @@
 import type AuthResponse from '../entity/AuthResponse'
 import type IAuthService from '../interface/IAuthService'
 import type IPersonRepository from '../interface/IPersonRepository'
-import { type IJsonWebTokenGenerator } from '../interface/IJsonWebToken'
+import { type IJsonWebToken } from '../interface/IJsonWebToken'
 import { type IErrorFactory } from '../interface/IErrorFactory'
 
 class AuthService implements IAuthService {
-  constructor (readonly errorFactory: IErrorFactory, readonly personRepository: IPersonRepository, readonly jsonWebTokenGenerator: IJsonWebTokenGenerator) {}
+  constructor (readonly errorFactory: IErrorFactory, readonly personRepository: IPersonRepository, readonly jsonWebTokenGenerator: IJsonWebToken) {}
 
   async authenticate (login: string, password: string): Promise<AuthResponse> {
     const person = await this.personRepository.getByEmail(login)
