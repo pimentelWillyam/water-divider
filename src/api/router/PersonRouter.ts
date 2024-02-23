7e29328a27172542aa11660bf0218c374ba2afd6
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-22 21:42:17 -0300

    refactor: refactoring person router structure

--- src/api/router/PersonRouter.ts
+++ src/api/router/PersonRouter.ts
@@ -5,28 +5,19 @@ import { type Router, type Request, type Response } from 'express'
 // importando service da rota
 import type IPersonController from '../interface/IPersonController'
 import type IPersonRouter from '../interface/IPersonRouter'
+import { type IAuthenticationMiddleware } from '../type/IAuthenticationMiddleware'
 
 // criando rotas
 
 class PersonRouter implements IPersonRouter {
   readonly routes: Router
-  constructor (readonly personController: IPersonController) {
+  constructor (readonly authenticationMiddleware: IAuthenticationMiddleware, readonly personController: IPersonController) {
     this.routes = express.Router()
-    this.routes.post('/person', (req: Request, res: Response) => {
-      void personController.create(req, res)
-    })
-    this.routes.get('/person', (req: Request, res: Response) => {
-      void personController.getAll(res)
-    })
-    this.routes.get('/person/:id', (req: Request, res: Response) => {
-      void personController.get(req, res)
-    })
-    this.routes.put('/person/:id', (req: Request, res: Response) => {
-      void personController.update(req, res)
-    })
-    this.routes.delete('/person/:id', (req: Request, res: Response) => {
-      void personController.delete(req, res)
-    })
+    this.routes.post('/person', this.authenticationMiddleware.authenticateToken, (req: Request, res: Response) => { void this.personController.create(req, res) })
+    this.routes.get('/person', this.authenticationMiddleware.authenticateToken, (res: Response) => { void this.personController.getAll(res) })
+    this.routes.get('/person/:id', this.authenticationMiddleware.authenticateToken, (req: Request, res: Response) => { void this.personController.get(req, res) })
+    this.routes.put('/person/:id', this.authenticationMiddleware.authenticateToken, (req: Request, res: Response) => { void this.personController.update(req, res) })
+    this.routes.delete('/person/:id', this.authenticationMiddleware.authenticateToken, (req: Request, res: Response) => { void this.personController.delete(req, res) })
   }
 }
 
