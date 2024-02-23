e3d424e4ae312675ab1e7235135f05221af895c0
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-23 10:19:12 -0300

    refactor: fixing dependency inversion principle

--- src/api/router/PersonRouter.ts
+++ src/api/router/PersonRouter.ts
@@ -13,11 +13,11 @@ class PersonRouter implements IPersonRouter {
   readonly routes: Router
   constructor (readonly authenticationMiddleware: IAuthenticationMiddleware, readonly personController: IPersonController) {
     this.routes = express.Router()
-    this.routes.post('/person', this.authenticationMiddleware.authenticateToken, (req: Request, res: Response) => { void this.personController.create(req, res) })
-    this.routes.get('/person', this.authenticationMiddleware.authenticateToken, (req: Request, res: Response) => { void this.personController.getAll(res) })
-    this.routes.get('/person/:id', this.authenticationMiddleware.authenticateToken, (req: Request, res: Response) => { void this.personController.get(req, res) })
-    this.routes.put('/person/:id', this.authenticationMiddleware.authenticateToken, (req: Request, res: Response) => { void this.personController.update(req, res) })
-    this.routes.delete('/person/:id', this.authenticationMiddleware.authenticateToken, (req: Request, res: Response) => { void this.personController.delete(req, res) })
+    this.routes.post('/person', this.authenticationMiddleware.authenticateToken.bind(this.authenticationMiddleware), (req: Request, res: Response) => { void this.personController.create(req, res) })
+    this.routes.get('/person', this.authenticationMiddleware.authenticateToken.bind(this.authenticationMiddleware), (req: Request, res: Response) => { void this.personController.getAll(res) })
+    this.routes.get('/person/:id', this.authenticationMiddleware.authenticateToken.bind(this.authenticationMiddleware), (req: Request, res: Response) => { void this.personController.get(req, res) })
+    this.routes.put('/person/:id', this.authenticationMiddleware.authenticateToken.bind(this.authenticationMiddleware), (req: Request, res: Response) => { void this.personController.update(req, res) })
+    this.routes.delete('/person/:id', this.authenticationMiddleware.authenticateToken.bind(this.authenticationMiddleware), (req: Request, res: Response) => { void this.personController.delete(req, res) })
   }
 }
 
