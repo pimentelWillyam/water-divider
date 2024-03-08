e41d0fea04aa4d45dbf394318aeaf2fd8e683080
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-03-08 14:50:22 -0300

    feat: sending email after login

--- src/api/service/AuthService.ts
+++ src/api/service/AuthService.ts
@@ -3,12 +3,14 @@ import type IAuthService from '../interface/IAuthService'
 import type IPersonRepository from '../interface/IPersonRepository'
 import { type IJsonWebToken } from '../interface/IJsonWebToken'
 import { type IErrorFactory } from '../interface/IErrorFactory'
+import { type IEmail } from '../interface/IEmail'
 
 class AuthService implements IAuthService {
-  constructor (readonly errorFactory: IErrorFactory, readonly personRepository: IPersonRepository, readonly jsonWebTokenGenerator: IJsonWebToken) {}
+  constructor (readonly errorFactory: IErrorFactory, readonly personRepository: IPersonRepository, readonly jsonWebTokenGenerator: IJsonWebToken, private readonly email: IEmail) {}
 
   async authenticate (login: string, password: string): Promise<AuthResponse> {
     const person = await this.personRepository.getByEmail(login)
+    this.email.send(person?.email as string, 'Sua conta foi logada', 'Nesse momento houve um login na sua conta, se não foi efetuado por você é recomendável que mude a sua senha para evitar que volte a acontecer')
     return {
       id: person?.id as string,
       name: person?.name as string,
