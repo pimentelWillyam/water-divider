703403af56b01c506c601e805545ad45241ddb8f
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-03-11 07:50:56 -0300

    feat: adding email sender at helper factory

--- src/api/factory/HelperFactory.ts
+++ src/api/factory/HelperFactory.ts
@@ -3,13 +3,17 @@ import { type IHelperFactory } from '../interface/IHelperFactory'
 import { type Helper } from '../type/Helper'
 import { type HelperType } from '../type/HelperType'
 import { config } from '../../config'
+import { Email } from '../type/EmailSender'
+
 class HelperFactory implements IHelperFactory {
   fabricate = (helperType: HelperType): Helper => {
     switch (helperType) {
       case 'json web token': return new JsonWebToken(config.jwt)
+      case 'email': return new Email(config.email)
+
       default: throw new Error('Tipo de helper inválido')
     }
   }
 }
 
-export type { HelperFactory }
+export { HelperFactory }
