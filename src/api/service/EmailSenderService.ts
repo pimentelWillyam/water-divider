6ff0931d4dcb1f370eb82f219fed36433f78a76d
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-03-18 02:37:29 -0300

    refactor: changing email change message

--- src/api/service/EmailSenderService.ts
+++ src/api/service/EmailSenderService.ts
@@ -6,7 +6,12 @@ class EmailSenderService implements IEmailSenderService {
   constructor (readonly email: IEmail) {}
 
   async sendPasswordChangeEmail (person: Person): Promise<void> {
-    this.email.send(person.email, 'Troca de senha', `Para trocar a sua senha utilize a seguinte rota: localhost:4000/api/person/${person.id}.,`)
+    this.email.send(person.email, 'Troca de senha', `Para trocar a sua senha utilize a seguinte rota: localhost:4000/api/person/${person.id} utilizando o verbo PATCH. Para tal, use o Insomnia ou algum cliente de requisições de seu gosto.
+    
+    O formato da requisição deve ser: 
+    {
+      "password": "SuaNovaSenha"
+    }`)
   }
 }
 
