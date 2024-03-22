29a826af3b1b76a115b247e7ac9d8114c9a91309
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-03-22 12:51:30 -0300

    fix: fixing memory datasource test

--- src/data/memoryDataSource.test.ts
+++ src/data/memoryDataSource.test.ts
@@ -3,7 +3,7 @@ import MemoryDataSource from './MemoryDataSource'
 
 test('Deve ser possível cadastrar uma pessoa', async () => {
   const memoryDataSource = new MemoryDataSource()
-  const person = await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
+  const person = await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', password: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
   expect(person.name).toBe('willyam')
   expect(person.email).toBe('willyampimenteldev@gmail.com')
   expect(person.age).toBe(22)
@@ -11,7 +11,7 @@ test('Deve ser possível cadastrar uma pessoa', async () => {
 
 test('Deve ser possível buscar uma lista com as pessoas cadastrada', async () => {
   const memoryDataSource = new MemoryDataSource()
-  const person = await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
+  const person = await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', password: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
   const personList = await memoryDataSource.fetchEveryPersonRegistry()
   expect(person.name).toBe(personList[0].name)
   expect(person.email).toBe(personList[0].email)
@@ -20,8 +20,8 @@ test('Deve ser possível buscar uma lista com as pessoas cadastrada', async () =
 
 test('Deve ser possível buscar uma pessoa cadastrada', async () => {
   const memoryDataSource = new MemoryDataSource()
-  const person = await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
-  const fetchedPerson = await memoryDataSource.fetchPersonBy('id', 'aaaaaaaaa') as Person
+  const person = await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', password: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
+  const fetchedPerson = await memoryDataSource.fetchPersonRegistryBy('id', 'aaaaaaaaa') as Person
   expect(person.name).toBe(fetchedPerson.name)
   expect(person.email).toBe(fetchedPerson.email)
   expect(person.age).toBe(fetchedPerson.age)
@@ -29,8 +29,8 @@ test('Deve ser possível buscar uma pessoa cadastrada', async () => {
 
 test('Deve ser possível atualizar uma pessoa cadastrada', async () => {
   const memoryDataSource = new MemoryDataSource()
-  await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
-  const updatedPerson = await memoryDataSource.updatePersonBy('id', 'aaaaaaaaa', { id: 'aaa', name: 'pimentel', age: 24, email: 'pimentelwillyamdev@gmail.com' }) as Person
+  await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', password: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
+  const updatedPerson = await memoryDataSource.updatePersonRegistryBy('id', 'aaaaaaaaa', { id: 'aaa', name: 'pimentel', password: 'pimentel', age: 24, email: 'pimentelwillyamdev@gmail.com' }) as Person
   expect(updatedPerson.name).toBe('pimentel')
   expect(updatedPerson.age).toBe(24)
   expect(updatedPerson.email).toBe('pimentelwillyamdev@gmail.com')
@@ -38,8 +38,8 @@ test('Deve ser possível atualizar uma pessoa cadastrada', async () => {
 
 test('Deve ser possível deletar uma pessoa cadastrada', async () => {
   const memoryDataSource = new MemoryDataSource()
-  await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
-  const deletedPerson = await memoryDataSource.deletePersonBy('id', 'aaaaaaaaa') as Person
+  await memoryDataSource.insertPersonRegistry({ id: 'aaaaaaaaa', name: 'willyam', password: 'willyam', email: 'willyampimenteldev@gmail.com', age: 22 })
+  const deletedPerson = await memoryDataSource.deletePersonRegistryBy('id', 'aaaaaaaaa') as Person
   expect(deletedPerson.name).toBe('willyam')
   expect(deletedPerson.email).toBe('willyampimenteldev@gmail.com')
   expect(deletedPerson.age).toBe(22)
