b342ede7c88fe494f34b7ce5b8dde60f138e8cb5
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-03-22 12:51:16 -0300

    fix: fixing personservice test

--- src/api/service/personService.test.ts
+++ src/api/service/personService.test.ts
@@ -12,7 +12,7 @@ test('Deve ser possível cadastrar uma pessoa', async () => {
   const nameNormalizer = new NameNormalizer()
   const personService = new PersonService(personRepository, idGenerator, nameNormalizer)
 
-  const person = await personService.create('willyam', 'willyampimenteldev@gmail.com', 22)
+  const person = await personService.create('willyam', 'willyam', 'willyampimenteldev@gmail.com', 22)
   expect(person.name).toBe('Willyam')
   expect(person.email).toBe('willyampimenteldev@gmail.com')
   expect(person.age).toBe(22)
@@ -24,7 +24,7 @@ test('Deve ser possível buscar uma lista com as pessoas cadastrada', async () =
   const idGenerator = new UUIDGenerator()
   const nameNormalizer = new NameNormalizer()
   const personService = new PersonService(personRepository, idGenerator, nameNormalizer)
-  const person = await personService.create('willyam', 'willyampimenteldev@gmail.com', 22)
+  const person = await personService.create('willya', 'willyam', 'willyampimenteldev@gmail.com', 22)
   const personList = await personService.getAll()
   expect(person.name).toBe(personList[0].name)
   expect(person.email).toBe(personList[0].email)
@@ -37,7 +37,7 @@ test('Deve ser possível buscar uma pessoa cadastrada', async () => {
   const idGenerator = new UUIDGenerator()
   const nameNormalizer = new NameNormalizer()
   const personService = new PersonService(personRepository, idGenerator, nameNormalizer)
-  const person = await personService.create('willyam', 'willyampimenteldev@gmail.com', 22)
+  const person = await personService.create('willyam', 'willyam', 'willyampimenteldev@gmail.com', 22)
   const fetchedPerson = await personService.get(person.id) as Person
   expect(person.name).toBe(fetchedPerson.name)
   expect(person.email).toBe(fetchedPerson.email)
@@ -50,8 +50,8 @@ test('Deve ser possível atualizar uma pessoa cadastrada', async () => {
   const idGenerator = new UUIDGenerator()
   const nameNormalizer = new NameNormalizer()
   const personService = new PersonService(personRepository, idGenerator, nameNormalizer)
-  const person = await personService.create('willyam', 'willyampimenteldev@gmail.com', 22)
-  const updatedPerson = await personService.update(person.id, { id: '', name: 'pimentel', email: 'pimentelwillyamdev@gmail.com', age: 24 }) as Person
+  const person = await personService.create('willyam', 'willyam', 'willyampimenteldev@gmail.com', 22)
+  const updatedPerson = await personService.update(person.id, { id: '', password: 'pimentel', name: 'pimentel', email: 'pimentelwillyamdev@gmail.com', age: 24 }) as Person
 
   expect(updatedPerson.name).toBe('Pimentel')
   expect(updatedPerson.age).toBe(24)
@@ -64,7 +64,7 @@ test('Deve ser possível deletar uma pessoa cadastrada', async () => {
   const idGenerator = new UUIDGenerator()
   const nameNormalizer = new NameNormalizer()
   const personService = new PersonService(personRepository, idGenerator, nameNormalizer)
-  const person = await personService.create('willyam', 'willyampimenteldev@gmail.com', 22)
+  const person = await personService.create('willyam', 'willyam', 'willyampimenteldev@gmail.com', 22)
   const deletedPerson = await personService.delete(person.id) as Person
   expect(deletedPerson.name).toBe('Willyam')
   expect(deletedPerson.email).toBe('willyampimenteldev@gmail.com')
